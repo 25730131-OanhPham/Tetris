7c20ca0193b1c525303c709fa2278b8f6b0718ff
24730093-ChungDuong <24730093@ms.uit.edu.vn>
2026-05-15 14:15:38 +0700

    Thay đổi cách hiển thị block

--- tetris.cpp
+++ tetris.cpp
@@ -5,14 +5,18 @@
 #include <windows.h>
 
 using namespace std;
-#define H 20
-#define W 15
 
-char board[H][W] = {};
+#define H 22
+#define W 16
+
+char board[H][W];
 
 int x, y, b;
-char blocks[][4][4] ={
-        // I-block (hình thẳng - dọc)
+
+// ================= BLOCKS =================
+char blocks[][4][4] =
+{
+    // I-block (hình thẳng - dọc)
         {{' ','I',' ',' '},
          {' ','I',' ',' '},
          {' ','I',' ',' '},
@@ -53,69 +57,213 @@ char blocks[][4][4] ={
          {'L','L','L',' '},
          {' ',' ',' ',' '}}
 };
-bool canMove(int dx, int dy){
-    for (int i = 0; i < 4; i++ )
-        for (int j = 0; j < 4; j++ )
-            if (blocks[b][i][j] != ' ') {
-                int xt = x + j + dx;
-                int yt = y + i + dy;
-                if (xt < 1 || xt >= W-1 || yt < 1 || yt >= H-1 ) return false;
-                if (board[yt][xt] != ' ') return false;
-            }
-    return true;
+void setColor(int color)
+{
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
 }
-void block2Board(){
-    for (int i = 0; i < 4; i++ )
-        for (int j = 0; j < 4; j++ )
-            if (blocks[b][i][j] != ' ')
-                board[y+i][x+j] = blocks[b][i][j];
+
+void initBoard()
+{
+    for(int i=0;i<H;i++)
+    {
+        for(int j=0;j<W;j++)
+        {
+            if(i==0 || i==H-1 || j==0 || j==W-1)
+                board[i][j] = '*';
+            else
+                board[i][j] = ' ';
+        }
+    }
 }
-void boardDelBlock(){
-    for (int i = 0; i < 4; i++ )
-        for (int j = 0; j < 4; j++ )
-            if (blocks[b][i][j] != ' ')
-                board[y+i][x+j] = ' ';
-}
-void initBoard(){
-    for (int i = 0 ; i < H ; i++)
-        for (int j = 0 ; j < W ; j++)
-            if (i == 0 || i == H-1 || j ==0 || j == W-1) board[i][j] = '#';
-            else board[i][j] = ' ';
-}
-void draw(){
+
+void draw()
+{
     system("cls");
 
-    for (int i = 0 ; i < H ; i++, cout<<endl)
-        for (int j = 0 ; j < W ; j++) cout<<board[i][j];
+    for(int i=0;i<H;i++)
+    {
+        for(int j=0;j<W;j++)
+        {
+            char c = board[i][j];
+
+            // Viền
+            if(c == '*')
+            {
+                setColor(15);
+                cout << "+";
+            }
+
+            // Block
+            else if(c != ' ')
+            {
+                setColor(15);
+                cout << char(219) << char(219);
+            }
+
+            // Khoảng trống
+            else
+            {
+                cout << "  ";
+            }
+        }
+
+        cout << endl;
+    }
+
+    setColor(7);
+
+    cout << endl;
+    cout << "A: Left  D: Right  S: Down  Q: Quit" << endl;
+}
+
+bool canMove(int dx, int dy)
+{
+    for(int i=0;i<4;i++)
+    {
+        for(int j=0;j<4;j++)
+        {
+            if(blocks[b][i][j] != ' ')
+            {
+                int newX = x + j + dx;
+                int newY = y + i + dy;
+
+                if(newX <= 0 || newX >= W-1 ||
+                   newY <= 0 || newY >= H-1)
+                    return false;
+
+                if(board[newY][newX] != ' ')
+                    return false;
+            }
+        }
+    }
+
+    return true;
+}
+void placeBlock()
+{
+    for(int i=0;i<4;i++)
+    {
+        for(int j=0;j<4;j++)
+        {
+            if(blocks[b][i][j] != ' ')
+            {
+                board[y+i][x+j] = blocks[b][i][j];
+            }
+        }
+    }
+}
+
+void removeBlock()
+{
+    for(int i=0;i<4;i++)
+    {
+        for(int j=0;j<4;j++)
+        {
+            if(blocks[b][i][j] != ' ')
+            {
+                board[y+i][x+j] = ' ';
+            }
+        }
+    }
+}
+
+void clearLines()
+{
+    for(int i=1;i<H-1;i++)
+    {
+        bool full = true;
+
+        for(int j=1;j<W-1;j++)
+        {
+            if(board[i][j] == ' ')
+            {
+                full = false;
+                break;
+            }
+        }
+
+        if(full)
+        {
+            for(int k=i;k>1;k--)
+            {
+                for(int j=1;j<W-1;j++)
+                {
+                    board[k][j] = board[k-1][j];
+                }
+            }
+
+            for(int j=1;j<W-1;j++)
+            {
+                board[1][j] = ' ';
+            }
+        }
+    }
 }
 
 int main()
 {
     srand(time(0));
-    x = 5; y = 0; b = rand()%7;
+
     initBoard();
-    while (1){
-        boardDelBlock();
-        if (kbhit()){
+
+    x = 5;
+    y = 1;
+    b = rand()%8;
+
+    while(true)
+    {
+        removeBlock();
+        
+        if(kbhit())
+        {
             char c = getch();
-            if ((c == 'a' || c == 'A') && canMove(-1,0)) x--;
-            if ((c == 'd' || c == 'D') && canMove( 1,0)) x++;
-            if ((c == 'x' || c == 'X') && canMove( 0,1)) y++;
-            if (c == 'q' || c == 'Q') break;
+
+            if((c=='a' || c=='A') && canMove(-1,0))
+                x--;
+
+            if((c=='d' || c=='D') && canMove(1,0))
+                x++;
+
+            if((c=='s' || c=='S') && canMove(0,1))
+                y++;
+
+            if(c=='q' || c=='Q')
+                break;
         }
-        if (canMove(0,1)) y++;
-        else{
-            block2Board();
-            x = 5; y = 0; b = rand()%7;
-            if (!canMove(0, 1)) {
+        if(canMove(0,1))
+        {
+            y++;
+        }
+        else
+        {
+            placeBlock();
+
+            clearLines();
+
+            x = 5;
+            y = 1;
+            b = rand()%8;
+
+            if(!canMove(0,0))
+            {
                 system("cls");
-                cout << "Game Over!" << endl;
+
+                setColor(12);
+                cout << "\n\n";
+                cout << "        GAME OVER!\n";
+
+                setColor(7);
+
                 break;
             }
         }
-        block2Board();
+
+        placeBlock();
+
         draw();
-        Sleep(150);
+
+        Sleep(180);
     }
+
     return 0;
 }
